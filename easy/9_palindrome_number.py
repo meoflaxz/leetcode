a631c71119833e1817bee30c3f42694242793edc
Meor Hasyim <40785172+meoflaxz@users.noreply.github.com>
2025-10-10 18:53:06 +0800

    base question 9 palindrome

---
+++ easy/9_palindrome_number.py
@@ -0,0 +1,11 @@
+# Gives and integer x, return true if x is a palindrome, and false otherwise.
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x == True:
+            return True
+        return x
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.isPalindrome(121))
